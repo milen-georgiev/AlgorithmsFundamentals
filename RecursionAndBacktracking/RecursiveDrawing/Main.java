c1e62f8e6297d55ffcd0a19d1d4d3d745035d2fa
milen-georgiev <69892213+milen-georgiev@users.noreply.github.com>
2022-06-15 16:08:34 +0300

    RecursiveDrawing

--- RecursionAndBacktracking/RecursiveDrawing/Main.java
+++ RecursionAndBacktracking/RecursiveDrawing/Main.java
@@ -1,8 +1,34 @@
 package RecursionAndBacktracking.RecursiveDrawing;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+        calculateFactorial(n);
+    }
+
+    private static void calculateFactorial(int n) {
+
+        printFigure("*", n);
+        if (n > 1) {
+        calculateFactorial(n - 1);
+        }
+        printFigure("#", n);
+
+
+    }
+
+    private static void printFigure(String symbol, int step) {
+        for (int i = 1; i <= step; i++) {
+            System.out.print(symbol);
+        }
+        System.out.println();
 
     }
 }
