4ad28de3cd198a241391e609d58dcee39df7f06c
milen-georgiev <69892213+milen-georgiev@users.noreply.github.com>
2022-06-15 15:51:46 +0300

    Recursive Factorial

--- RecursionAndBacktracking/RecursiveFactorial/Main.java
+++ RecursionAndBacktracking/RecursiveFactorial/Main.java
@@ -1,8 +1,24 @@
 package RecursionAndBacktracking.RecursiveFactorial;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+        System.out.println(calculateFactorial(n));
     }
+
+    private static long calculateFactorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        return n * calculateFactorial(n - 1);
+    }
+
+
 }
