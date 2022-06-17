1f7952c5d8938f18397eb9044c6a9e6f3694c692
milen-georgiev <69892213+milen-georgiev@users.noreply.github.com>
2022-06-17 16:35:46 +0300

    RecursiveFibonacci

--- RecursionAndBacktracking/RecursiveFibonacci/Main.java
+++ RecursionAndBacktracking/RecursiveFibonacci/Main.java
@@ -3,10 +3,27 @@ package RecursionAndBacktracking.RecursiveFibonacci;
 import java.util.Scanner;
 
 public class Main {
+    public static long [] memoryForFib;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
+        Scanner scan = new Scanner(System.in);
+        
+        int n = Integer.parseInt(scan.nextLine());
+        memoryForFib = new long[n + 1];
+        System.out.println(fib(n));
 
+    }
+
+    private static long fib(int n) {
+
+        if (n <= 1) {
+            return 1;
+        }
+
+        if (memoryForFib[n] != 0) {
+            return memoryForFib[n];
+        }
+
+        return  memoryForFib[n] = fib(n - 1) + fib(n - 2);
     }
 
 }
