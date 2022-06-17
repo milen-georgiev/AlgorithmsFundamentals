68492b50d06043aecbdbb783c3e9bd8678c840f6
milen-georgiev <69892213+milen-georgiev@users.noreply.github.com>
2022-06-17 15:51:45 +0300

    QueensPuzzle

--- RecursionAndBacktracking/QueensPuzzle/Main.java
+++ RecursionAndBacktracking/QueensPuzzle/Main.java
@@ -1,12 +1,105 @@
 package RecursionAndBacktracking.QueensPuzzle;
 
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
 
+    public static char[][] table = {
+            {'-', '-', '-', '-', '-', '-', '-', '-'},
+            {'-', '-', '-', '-', '-', '-', '-', '-'},
+            {'-', '-', '-', '-', '-', '-', '-', '-'},
+            {'-', '-', '-', '-', '-', '-', '-', '-'},
+            {'-', '-', '-', '-', '-', '-', '-', '-'},
+            {'-', '-', '-', '-', '-', '-', '-', '-'},
+            {'-', '-', '-', '-', '-', '-', '-', '-'},
+            {'-', '-', '-', '-', '-', '-', '-', '-'}
+    };
+
+    public static void main(String[] args) {
+
+        findQueenPositions(0);
     }
 
+    private static void findQueenPositions(int row) {
+        if (row == 8) {
+            printSolution();
+        } else {
+            for (int col = 0; col < 8; col++) {
+                if (canPlaceQueen(row, col)) {
+                    putQueen(row, col);
+                    findQueenPositions(row + 1);
+                    removeQueen(row, col);
+                }
+            }
+        }
+    }
+
+    private static void removeQueen(int row, int col) {
+        table[row][col] = '-';
+    }
+
+    private static void putQueen(int row, int col) {
+        table[row][col] = '*';
+    }
+
+    private static boolean canPlaceQueen(int row, int col) {
+        for (int c = 0; c < 8; c++) {
+            if (table[row][c] == '*') {
+                return false;
+            }
+        }
+
+        for (int r = 0; r < 8; r++) {
+            if (table[r][col] == '*') {
+                return false;
+            }
+        }
+
+        int r = row;
+        int c = col;
+
+        while (r >= 0 && c >= 0) {
+            if (table[r--][c--] == '*') {
+                return false;
+            }
+        }
+
+        r = row;
+        c = col;
+
+        while (r < 8 && c < 8) {
+            if (table[r++][c++] == '*') {
+                return false;
+            }
+        }
+
+        r = row;
+        c = col;
+
+        while (r >= 0 && c < 8) {
+            if (table[r--][c++] == '*') {
+                return false;
+            }
+        }
+
+        r = row;
+        c = col;
+
+        while (r < 8 && c >= 0) {
+            if (table[r++][c--] == '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private static void printSolution() {
+        for (char[] chars : table) {
+            for (char symbol : chars) {
+                System.out.print(symbol + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
