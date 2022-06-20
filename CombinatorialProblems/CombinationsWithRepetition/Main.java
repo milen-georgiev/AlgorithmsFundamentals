201c5f0bd4f6a015f29a99a56d9e88a9161abc2e
milen-georgiev <69892213+milen-georgiev@users.noreply.github.com>
2022-06-20 11:23:00 +0300

    CombinationsWithRepetition

--- CombinatorialProblems/CombinationsWithRepetition/Main.java
+++ CombinatorialProblems/CombinationsWithRepetition/Main.java
@@ -9,36 +9,27 @@ public class Main {
     public static boolean[] used;
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+
         input = scanner.nextLine().split("\\s+");
 
         int k = Integer.parseInt(scanner.nextLine());
 
         variations = new String[k];
 
-        used = new boolean[input.length];
-
-        variation(0);
+        combinations(0, 0);
     }
 
-    private static void variation(int index) {
+    private static void combinations(int index, int start) {
         if (index == variations.length) {
             print(variations);
-            return;
+        } else {
+            for (int i = start; i < input.length; i++) {
+                variations[index] = input[i];
+                combinations(index + 1, i);
+            }
         }
 
-        for (int i = 0; i < input.length; i++) {
-            variations[index] = input[i];
-            variation(index + 1);
-        }
-
-    }
-
-    private static void swap(String[] input, int first, int second) {
-        String temp = input[first];
-        input[first] = input[second];
-        input[second] = temp;
     }
 
     private static void print(String[] input) {
