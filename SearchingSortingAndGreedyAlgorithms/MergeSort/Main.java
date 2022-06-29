871ef2fc11866b3a2d17b982f0ec26dd220172fe
milen-georgiev <69892213+milen-georgiev@users.noreply.github.com>
2022-06-29 10:04:34 +0300

    MergeSort

---
+++ SearchingSortingAndGreedyAlgorithms/MergeSort/Main.java
@@ -0,0 +1,36 @@
+package SearchingSortingAndGreedyAlgorithms.MergeSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        sort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;
+                }
+            }
+            swap(arr, i, index);
+        }
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
