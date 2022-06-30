b33be6d83cd5f7e3e6b1c4f32034484073b63001
milen-georgiev <69892213+milen-georgiev@users.noreply.github.com>
2022-06-30 18:42:03 +0300

    SumOfCoins

--- SearchingSortingAndGreedyAlgorithms/MergeSort/Main.java
+++ SearchingSortingAndGreedyAlgorithms/MergeSort/Main.java
@@ -12,25 +12,56 @@ public class Main {
                 .toArray();
 
         sort(arr);
-        for (int i : arr) {
-            System.out.print(i + " ");
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int number : arr) {
+            builder.append(number).append(" ");
         }
-    }
-    public static void sort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int index = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[index]) {
-                    index = j;
-                }
-            }
-            swap(arr, i, index);
-        }
+        System.out.println(builder.toString());
     }
 
-    private static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+    private static void sort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    private static void mergeSort(int[] arr, int begin, int end) {
+        if (begin >= end) {
+            return;
+        }
+
+        int mid = (begin + end) / 2;
+
+        mergeSort(arr, begin, mid);
+        mergeSort(arr, mid + 1, end);
+
+        merge(arr, begin, mid, end);
+    }
+
+    private static void merge(int[] arr, int begin, int mid, int end) {
+        if (mid < 0 || mid >= arr.length || arr[mid] < arr[mid + 1]) {
+            return;
+        }
+
+        int left = begin;
+        int right = mid + 1;
+        int[] helper = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            helper[i] = arr[i];
+        }
+
+
+        for (int i = begin; i <= end; i++) {
+            if (left > mid) {
+                arr[i] = helper[right++];
+            } else if (right > end) {
+                arr[i] = helper[left++];
+            } else if (helper[left] < helper[right]) {
+                arr[i] = helper[left++];
+            } else {
+                arr[i] = helper[right++];
+            }
+        }
     }
 }
---
+++ SearchingSortingAndGreedyAlgorithms/Quicksort/Main.java
@@ -0,0 +1,9 @@
+package SearchingSortingAndGreedyAlgorithms.Quicksort;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
